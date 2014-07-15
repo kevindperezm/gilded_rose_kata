b77c5e1d3a69d5ca7de13cfc158cb10ef681c824
Kevin Perez <kevindperezm@gmail.com>
2014-07-15 11:25:36 -0500

    change to use new constants

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -31,7 +31,7 @@ def update_quality(items)
       item.sell_in -= 1
     end
     if item.sell_in < 0
-      if item.name != "Aged Brie"
+      if item.name != AGED_BRIE_NAME
         if item.name != PASSES_NAME
           if item.quality > 0
             if item.name != SULFURAS_NAME
