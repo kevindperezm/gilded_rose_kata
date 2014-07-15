438dd3997c94d32684d31ba8146216ecdad11ff3
Kevin Perez <kevindperezm@gmail.com>
2014-07-15 10:47:49 -0500

    extract method update_quality_for_backstage_passes

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -21,16 +21,20 @@ def update_quality_for_antique(item)
   if item.quality < 50
     item.quality += 1
     if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-      if item.sell_in < 11
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
-      if item.sell_in < 6
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
+      update_quality_for_backstage_passes(item)
+    end
+  end
+end
+
+def update_quality_for_backstage_passes(item)
+  if item.sell_in < 11
+    if item.quality < 50
+      item.quality += 1
+    end
+  end
+  if item.sell_in < 6
+    if item.quality < 50
+      item.quality += 1
     end
   end
 end
